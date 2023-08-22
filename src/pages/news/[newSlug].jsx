4cda26b788858f50bf7139e2c238ff58eaca79a7
Islom0906 <iabdugofurov99@gmail.com>
2023-08-22 08:17:40 +0500

    xato togirlandi

--- src/pages/news/[newSlug].jsx
+++ src/pages/news/[newSlug].jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import news1 from '/public/section-bg-1.jpg'
+import news1 from '/public/About-us/about us section 1.jpg'
 import Head from "next/head";
 
 
